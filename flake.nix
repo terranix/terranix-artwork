d5b263378fedaec0102156f18f5dfd03b08aa474
Ingolf Wagner <contact@ingolf-wagner.de>
2021-10-20 07:29:22 +0200

    add all favicons necessary

--- flake.nix
+++ flake.nix
@@ -16,12 +16,29 @@
           type = "app";
           program = toString (pkgs.writers.writeBash "favicon" ''
             cd terranix-logo-favicon
-            ${convert} ../terranix-logo.svg -resize 256x256 -transparent white favicon-256.png
-            ${convert} favicon-256.png -resize 16x16 favicon-16.png
-            ${convert} favicon-256.png -resize 32x32 favicon-32.png
-            ${convert} favicon-256.png -resize 64x64 favicon-64.png
-            ${convert} favicon-256.png -resize 128x128 favicon-128.png
-            ${convert} favicon-16.png favicon-32.png favicon-64.png favicon-128.png favicon-256.png -colors 256 favicon.ico
+            ${convert} ../terranix-logo.svg \
+            -thumbnail '256x256>' \
+            -gravity center \
+            -extent 256x256 \
+            -transparent white logo.png
+
+            ${convert} logo.png -define icon:auto-resize=64,48,32,16 favicon.ico
+
+            ${convert} logo.png -resize 57x57 apple-touch-icon.png
+            ${convert} logo.png -resize 57x57 apple-touch-icon-57x57.png
+            ${convert} logo.png -resize 72x72 apple-touch-icon-72x72.png
+            ${convert} logo.png -resize 76x76 apple-touch-icon-76x76.png
+            ${convert} logo.png -resize 114x114 apple-touch-icon-114x114.png
+            ${convert} logo.png -resize 120x120 apple-touch-icon-120x120.png
+            ${convert} logo.png -resize 144x144 apple-touch-icon-144x144.png
+            ${convert} logo.png -resize 152x152 apple-touch-icon-152x152.png
+            ${convert} logo.png -resize 180x180 apple-touch-icon-180x180.png
+
+            ${convert} logo.png -resize 192x192 android-icon-192x192.png
+
+            ${convert} logo.png -resize 150x150 mstile-150.png
+
+            rm -f logo.png
           '');
         };
         defaultApp = self.apps.${system}.favicon;
